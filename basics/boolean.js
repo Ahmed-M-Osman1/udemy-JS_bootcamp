4a2b9fea9b57283112a973b38ebbc6783d5512f2
Ahmed M.Osman <64021350+akiid777@users.noreply.github.com>
2021-10-05 15:25:32 +0200

    solved challange 14 booleans

---
+++ basics/boolean.js
@@ -0,0 +1,16 @@
+// === equality operator
+
+// !== not equal operator
+
+// < less than operator
+
+// > graeter than operator
+
+// <= less than or equal operator 
+
+// >= greater than or equal operator 
+
+let temp = 31 
+let isFreezing = temp <= 32
+
+console.log(isFreezing)
---
+++ basics/challange 14-Booleans and Comparison Operators.js
@@ -0,0 +1,8 @@
+let age = 26
+
+let isChiled = age <= 7 
+
+let isSenior = age >= 65
+
+console.log(isChiled)
+console.log(isSenior)
