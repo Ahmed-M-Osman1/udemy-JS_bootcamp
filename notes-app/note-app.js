77c2b0ccdf979504afdc798b24e58a4db60aab2f
akiid777 <akiid777@gmail.com>
2021-11-04 23:33:38 +0200

    add todo and note app

---
+++ notes-app/note-app.js
@@ -0,0 +1,33 @@
+const notes = [
+    {
+        title:'my next trip.',
+        body:'I would like to go to spain.'
+    },{
+        title:'Hobbits to work on.',
+        body:'Exercise, Eating a bit better.'
+    },{
+        title:'Office modification.',
+        body:'Get a new seats.'
+    }
+]
+
+/*
+//query and remove.
+// const p = document.querySelector('p')
+// p.remove()
+
+//query selector all 
+const p = document.querySelectorAll('p')
+p.forEach(function(p){
+    //p.textContent = 'My name Is ahmed'
+    //console.log(p.textContent)
+    // p.remove()
+})
+
+// add a new element
+const text = document.createElement('p')
+text.textContent = 'This is a new element from javascript.'
+document.querySelector('body').appendChild(text)
+*/
+
+document.querySelector('button').addEventListener('')
---
+++ todo-app/todo-app.js
@@ -0,0 +1,48 @@
+const todo = [
+  {
+    text: "Pray",
+    Completed: true,
+  },
+  {
+    text: "Read",
+    Completed: true,
+  },
+  {
+    text: "Learn",
+    Completed: false,
+  },
+  {
+    text: "Sleep",
+    Completed: false,
+  },
+  {
+    text: "Eat",
+    Completed: true,
+  },
+];
+
+// const p = document.querySelectorAll('p')
+
+// p.forEach(function(p){
+//     if (p.textContent.includes('the')){
+//         p.remove()
+//     }
+// })
+
+const leftTodo = function (todo) {
+  return todo.filter(function (todo) {
+    return todo.Completed === false;
+  });
+};
+
+// console.log(leftTodo(todo).length)
+
+const leftTodotext = document.createElement("h2");
+leftTodotext.textContent = ` There is ${leftTodo(todo).length} todos left`;
+document.querySelector("div").appendChild(leftTodotext);
+
+todo.forEach(function (todo) {
+  const leftTodo = document.createElement("p");
+  leftTodo.textContent = `${todo.text}`;
+  document.querySelector("body").appendChild(leftTodo);
+});
